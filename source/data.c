0daf683f0ae50456c525525e854cfc36574dae7e
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-10-02 18:35:51 +0100

    Add serialization and data modules.

--- source/data.c
+++ source/data.c
@@ -1,30 +1,39 @@
 #include "data.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct data_t *data_create(int size)
 {
-    struct data_t *data_s = malloc(sizeof(struct data_t));
-    data_s->datasize = size;
-    data_s->data = malloc(size);
-    return data_s;
+    struct data_t *new_data = malloc(sizeof(struct data_t));
+    new_data->datasize = size;
+    new_data->data = malloc(size);
+    return new_data;
 }
 
 struct data_t *data_create2(int size, void *data)
 {
-    struct data_t *data_s = malloc(sizeof(struct data_t));
-    data_s->datasize = size;
-    data_s->data = data;
-    return data_s;
+    struct data_t *new_data = malloc(sizeof(struct data_t));
+    new_data->datasize = size;
+    new_data->data = data;
+    return new_data;
 }
 
 void data_destroy(struct data_t *data)
 {
+    free(data->data);
+    free(data);
 }
 
 struct data_t *data_dup(struct data_t *data)
 {
+    struct data_t *new_data = data_create(data->datasize);
+    memcpy(new_data->data, data->data, data->datasize);
+    return new_data;
 }
 
 void data_replace(struct data_t *data, int new_size, void *new_data)
 {
+    free(data->data);
+    data->data = new_data;
+    data->datasize = new_size;
 }
--- source/serialization.c
+++ source/serialization.c
@@ -1,10 +1,72 @@
 #include "serialization.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int keyArray_to_buffer(char **keys, char **keys_buf)
 {
-    return -1;
+    int i;
+    int size = 0;
+    for (i = 0; keys[i] != NULL; i++)
+    {
+        size += strlen(keys[i]) + 1; // incluir '\0' de fim de string
+    }
+    *keys_buf = (char *)malloc(sizeof(int) + size);
+    memcpy(*keys_buf, &i, sizeof(int)); // nº de elementos no inicio do buf
+    memcpy(*keys_buf + sizeof(int), *keys, size);
+    return i;
 }
 
 char **buffer_to_keyArray(char *keys_buf, int keys_buf_size)
 {
+    char **keys = (char **)malloc(keys_buf_size * sizeof(char *));
+    int size = 0;
+    for (int i = 0; i < keys_buf_size; i++)
+    {
+        size = strlen(keys_buf) + 1;
+        keys[i] = (char *)malloc(strlen(keys_buf) + 1);
+        strcpy(keys[i], keys_buf);
+        keys_buf += size;
+    }
+    return keys;
+}
+
+void print_str_array(char **str_array)
+{
+    printf("[");
+    int i = 0;
+    for (i = 0; str_array[i] != NULL; i++)
+    {
+        if (str_array[i + 1] == NULL)
+        {
+            printf("%s", str_array[i]);
+            break;
+        }
+        printf("%s, ", str_array[i]);
+    }
+    printf("]\n");
+}
+
+/**
+ * Só para testes.
+ */
+int main(int argc, char const *argv[])
+{
+    char *keys[] = {"ola", "amigos", "fcul", "sd", NULL};
+    char **keys_buf = (char **)malloc(sizeof(char *));
+
+    // INICIALIZAR
+    printf("Array de chaves original: ");
+    print_str_array(keys);
+
+    // SERIALIZAR
+    int keys_buf_size = keyArray_to_buffer(keys, keys_buf);
+    // int keys_buf_size = (int)**keys_buf;
+    printf("Tamanho do buffer serializado: %d\n", keys_buf_size);
+
+    // DE-SERIALIZAR
+    char **final_keys = buffer_to_keyArray(*(keys_buf) + sizeof(int), keys_buf_size);
+    printf("Array de chaves de-serializado: ");
+    print_str_array(final_keys);
+    return 0;
 }
