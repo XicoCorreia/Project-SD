54b51c00f325c2b22ac794130ae650afa8d6e725
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-10-05 11:25:40 +0100

    Add error handling to data.

--- source/data.c
+++ source/data.c
@@ -1,18 +1,34 @@
 #include "data.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct data_t *data_create(int size)
 {
     struct data_t *new_data = malloc(sizeof(struct data_t));
+    if (new_data == NULL)
+    {
+        perror("data_create");
+        exit(1);
+    }
     new_data->datasize = size;
     new_data->data = malloc(size);
+    if (new_data->data == NULL)
+    {
+        perror("data_create");
+        exit(1);
+    }
     return new_data;
 }
 
 struct data_t *data_create2(int size, void *data)
 {
     struct data_t *new_data = malloc(sizeof(struct data_t));
+    if (new_data->data == NULL)
+    {
+        perror("data_create2");
+        exit(1);
+    }
     new_data->datasize = size;
     new_data->data = data;
     return new_data;
