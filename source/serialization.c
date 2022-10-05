a72d495dbb16b75cd55437f1865ff6576788d05d
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-10-05 15:01:33 +0100

    Refactor Makefile and serialization, add test_serialization.

--- source/serialization.c
+++ source/serialization.c
@@ -5,88 +5,50 @@
 
 int keyArray_to_buffer(char **keys, char **keys_buf)
 {
-    int i, size;
-    for (i = 0, size = sizeof(int); keys[i] != NULL; i++)
+    int keys_buf_size = 0;
+    for (int i = 0; keys[i] != NULL; i++)
     {
-        size += sizeof(int) + strlen(keys[i]) + 1;
+        keys_buf_size += sizeof(int) + strlen(keys[i]) + 1;
     }
-    *keys_buf = (char *)malloc(size);
-    memcpy(*keys_buf, &i, sizeof(int));
+    *keys_buf = malloc(keys_buf_size);
 
-    char *keys_buf_ptr = *keys_buf + sizeof(int);
-    for (i = 0; keys[i] != NULL; i++)
+    keys_buf_size = 0;
+    for (int i = 0; keys[i] != NULL; i++)
     {
         int keylen = strlen(keys[i]) + 1;
-        memcpy(keys_buf_ptr, &keylen, sizeof(int));
-        keys_buf_ptr += sizeof(int);
-        strcpy(keys_buf_ptr, keys[i]);
-        keys_buf_ptr += keylen;
+        memcpy(*keys_buf + keys_buf_size, &keylen, sizeof(int));
+        strcpy(*keys_buf + keys_buf_size + sizeof(int), keys[i]);
+        keys_buf_size += sizeof(int) + keylen;
     }
-    return size;
+    return keys_buf_size;
 }
 
 char **buffer_to_keyArray(char *keys_buf, int keys_buf_size)
 {
-    int n = *keys_buf;
-    keys_buf += sizeof(int);
-    char **keys = (char **)malloc((n + 1) * sizeof(char *));
+    if (keys_buf == NULL || keys_buf_size <= 0)
+        return NULL;
+
+    int n = 0;
+    int offset = 0;
+    char **keys;
+
+    while (offset < keys_buf_size)
+    {
+        offset += *(keys_buf + offset) + sizeof(int);
+        n++;
+    }
+
+    keys = malloc((n + 1) * sizeof(char *));
+    if (keys == NULL)
+        return NULL;
+
+    offset = 0;
     for (int i = 0; i < n; i++)
     {
-        int keylen = (int)*keys_buf;
-        keys_buf += sizeof(int);
-        keys[i] = (char *)malloc(keylen);
-        strcpy(keys[i], keys_buf);
-        keys_buf += keylen;
+        keys[i] = strdup(keys_buf + offset + sizeof(int));
+        offset += *(keys_buf + offset) + sizeof(int);
     }
     keys[n] = NULL;
+
     return keys;
 }
-
-void print_str_array(char **str_array)
-{
-    printf("[");
-    int i = 0;
-    for (i = 0; str_array[i] != NULL; i++)
-    {
-        if (str_array[i + 1] == NULL)
-        {
-            printf("%s", str_array[i]);
-            break;
-        }
-        printf("%s, ", str_array[i]);
-    }
-    printf("]\n");
-}
-
-// /**
-//  * Só para testes.
-//  */
-// int main(int argc, char const *argv[])
-// {
-//     char *keys[] = {"ola", "amigos", "fcul", "sd", NULL};
-//     char **keys_buf = (char **)malloc(sizeof(char *));
-
-//     // INICIALIZAR
-//     printf("Array de chaves original: ");
-//     print_str_array(keys);
-
-//     // SERIALIZAR
-//     int keys_buf_size = keyArray_to_buffer(keys, keys_buf);
-//     // int keys_buf_size = (int)**keys_buf;
-//     printf("Tamanho do buffer serializado: %d\n", keys_buf_size);
-
-//     // DE-SERIALIZAR
-//     char **final_keys = buffer_to_keyArray(*keys_buf, keys_buf_size);
-//     printf("Array de chaves de-serializado: ");
-//     print_str_array(final_keys);
-
-//     // CLEANUP
-//     free(*keys_buf);
-//     free(keys_buf);
-//     for (int i = 0; final_keys[i] != NULL; i++)
-//     {
-//         free(final_keys[i]);
-//     }
-//     free(final_keys);
-//     return 0;
-// }
