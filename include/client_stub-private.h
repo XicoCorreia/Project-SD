5722a2891a9b5f6cd9b17dbeb492ecd76680e184
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-10-16 18:11:01 +0100

    Add empty source files.

---
+++ include/client_stub-private.h
@@ -0,0 +1,16 @@
+#ifndef _CLIENT_STUB_PRIVATE_H
+#define _CLIENT_STUB_PRIVATE_H
+
+#include "client_stub.h"
+#include "tree.h"
+#include <sys/types.h>
+
+struct rtree_t
+{
+    // TODO
+    u_int16_t port;
+    char *address;
+    int socketfd;
+};
+
+#endif
---
+++ source/tree_skel.c
@@ -0,0 +1,49 @@
+#include "tree_skel.h"
+
+int tree_skel_init()
+{
+    // TODO
+    return -1;
+}
+
+void tree_skel_destroy()
+{
+}
+
+int invoke(struct message_t *msg)
+{
+    // TODO
+
+    // switch (msg->opcode)
+    // {
+    // case MESSAGE_T__OPCODE__OP_BAD:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_SIZE:
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_HEIGHT:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_DEL:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_GET:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_PUT:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_GETKEYS:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_GETVALUES:
+    //     /* code */
+    //     break;
+    // case MESSAGE_T__OPCODE__OP_ERROR:
+    //     /* code */
+    //     return -1;
+    // default:
+    //     return -1;
+    // }
+    return 0;
+}
