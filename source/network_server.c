1da834d83c321f927497e32e252d7144634c5e40
Filipeegipto <82580350+Filipeegipto@users.noreply.github.com>
2022-10-26 18:19:26 +0100

    Add files via upload

--- source/network_server.c
+++ source/network_server.c
@@ -1,26 +1,121 @@
 #include "network_server.h"
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<string.h>
+#include "inet.h"
+#include <netinet/in.h>
+#include <signal.h>
 
 int network_server_init(short port)
 {
-    return -1;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) 
+    {
+        perror("network_server_init");
+        return -1;
+    }
+
+    struct sockaddr_in my_soc;
+    my_soc.sin_family = AF_INET;
+    my_soc.sin_addr.s_addr = htonl(INADDR_ANY);
+    my_soc.sin_port = port;
+
+    int opt = 1;
+
+    int sets = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (sets < 0)
+    {
+        perror("network_server_init");
+        return -1;
+    }
+
+    if((bind(sockfd, (struct sockaddr *) &my_soc, sizeof(my_soc))) < 0) 
+    {
+        perror("network_server_init");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
 }
 
 int network_main_loop(int listening_socket)
 {
-    return -1;
+    if(listen(listening_socket, 0) < 0) 
+    {
+        perror("network_main_loop");
+        return -1;
+    }
+
+    sigaction(SIGPIPE, &(struct sigaction){SIG_IGN}, NULL);  //chega ?
+
+    struct sockaddr_in my_soc;
+    socklen_t addr_size;
+    int csock;
+
+    while((csock = accept(listening_socket, (struct sockaddr *) &my_soc, &addr_size)) != -1) 
+    {
+        MessageT *msg = network_receive(csock);
+        int res;
+        if ((res = invoke(msg)) != 0)
+        {
+            return res;  //erro
+        }
+        network_send(csock, msg);
+        if(close(csock) < 0)
+        {
+            perror("network_main_loop");
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
 MessageT *network_receive(int client_socket)
 {
-    return NULL;
+    MessageT msg;
+    message_t__init(&msg);
+    int len = message_t__get_packed_size(&msg);
+    void *buffer;
+    int size = read(client_socket, buffer, len);  //read_all
+    if (size != len)
+    {
+        perror("network_receive");
+        return NULL;
+    }
+    *(msg) = message_t__unpack(NULL, len, buffer);
+    return &msg;
 }
 
 int network_send(int client_socket, MessageT *msg)
 {
-    return -1;
+    int len = message_t__get_packed_size(msg);
+    void *buffer = malloc(len);
+    if (buffer == NULL)
+    {
+        perror("network_send");
+        return -1;
+    }
+    message_t__pack(msg, buffer);
+    message_t__free_unpacked(msg, NULL);
+    size = write(client_socket, buffer, len);  //write_all
+    if (size != len)
+    {
+        perror("network_send");
+        return -1;
+    }
+    free(buffer);
+    return 0;
 }
 
 int network_server_close()
 {
-    return -1;
+    if (close(sockfd) < 0)
+    {
+        perror("network_server_close");
+        return -1;
+    }
+    return 0;
 }
--- source/tree_server.c
+++ source/tree_server.c
@@ -3,11 +3,24 @@
 int main(int argc, char const *argv[])
 {
     /* Testar os argumentos de entrada */
+    if (argc != 2){
+        printf("Uso: ./tree-server <port>\n");
+        printf("Exemplo de uso: ./tree-server 12345\n");
+        return -1;
+    }
     /* inicialização da camada de rede */
-    int socket_de_escuta = network_server_init(45683);
-    tree_skel_init();
-    int result = network_main_loop(socket_de_escuta);
-    tree_skel_destroy();
-    // TODO
-    return 0;
+    int socket_de_escuta = network_server_init(htons(atoi(argv[1])));
+    if (socket_de_escuta != -1)
+    {
+        tree_skel_init();
+        int result = network_main_loop(socket_de_escuta);
+        tree_skel_destroy();
+        int close = network_server_close();
+        if (close != 0)
+        {
+            return close;
+        }
+        return result;
+    }
+    return -1;
 }
