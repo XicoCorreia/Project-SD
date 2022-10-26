e6f8cfc7c1080270d8b300cdd8f483cb9460e721
XicoCorreia <xicocorreia17@gmail.com>
2022-10-26 11:54:49 +0100

    Network client done

--- source/network_client.c
+++ source/network_client.c
@@ -12,13 +12,13 @@ int network_connect(struct rtree_t *rtree)
     int sockfd;
     struct sockaddr_in server;
 
-    // * Cria socket TCP
+    // Cria socket TCP
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Erro ao criar socket TCP");
         return -1;
     }
 
-    // * Preenche estrutura server para estabelecer conexão
+    // Preenche estrutura server para estabelecer conexão
     server.sin_family = AF_INET;
     server.sin_port = htons(rtree->port);
     if (inet_pton(AF_INET, rtree->address, &server.sin_addr) < 1) {
@@ -27,7 +27,7 @@ int network_connect(struct rtree_t *rtree)
         return -1;
     }
 
-    // * Estabelece conexão com o servidor definido em server
+    // Estabelece conexão com o servidor definido em server
     if (connect(sockfd,(struct sockaddr *)&server, sizeof(server)) < 0) {
         perror("Erro ao conectar-se ao servidor");
         close(sockfd);
@@ -41,7 +41,32 @@ int network_connect(struct rtree_t *rtree)
 
 MessageT *network_send_receive(struct rtree_t *rtree, MessageT *msg)
 {
-    return NULL;
+    
+    int sockfd = rtree->socketfd;
+    int nbytes;
+    sdmessage__init(&msg);                      // ? Necessário
+
+    // Serializar mensagem
+    int len = sdmessage__get_packet_size(&msg);
+    void* buf = malloc(len);
+    sdmessage__pack(&msg, buf);
+
+    // Enviar mensagem para o servidor
+    if((nbytes = write(sockfd,msg,len)) != len){
+        perror("Erro ao enviar os dados");
+        close(sockfd);
+        return NULL;
+    }
+
+    // Ler mensagem do servidor
+    if((nbytes = read(sockfd, buf, len)) != len) {
+        perror("Erro ao receber dados");
+        close(sockfd);
+        return -1;
+    }
+    // De-serializar mensagem recebida
+    msg = sdmessage__unpack(NULL,len,buf);      // ? Argumentos certos
+    return msg;
 }
 
 /* A função network_close() fecha a ligação estabelecida por
